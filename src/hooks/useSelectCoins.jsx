35fc1c0d40ad87fe00bac0863caa2be04144b923
vldvy <ald.jo.a@gmail.com>
2022-03-10 21:43:53 -0300

    fixinf font

--- src/hooks/useSelectCoins.jsx
+++ src/hooks/useSelectCoins.jsx
@@ -2,9 +2,9 @@ import { useState } from "react";
 import styled from "@emotion/styled";
 
 const Label = styled.label`
-  color: #fff;
+  color: #FFF;
   display: block;
-  font-family: 'Lato, sans-serif;
+  font-family: 'Lato', sans-serif;
   font-size: 24px;
   font-weight: 700;
   margin: 15px 0;
